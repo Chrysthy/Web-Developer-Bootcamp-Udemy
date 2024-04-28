732fc34ff06377890b65bbd4e510ba7fc38f452e
Chrystine Martins <chrystine.martins@outlook.com>
2024-04-28 13:28:24 -0300

    truth and false

---
+++ javascript/truth-y-false-y-values/index.js
@@ -0,0 +1,46 @@
+// const userInput = prompt("Enter something")
+
+// if (userInput) {
+//     console.log("Truthy");
+
+// } else {
+//     console.log("Falsy");
+// }
+
+
+if (0) {
+    console.log("Truthy");
+
+} else {
+    console.log("Falsy");
+}
+
+
+if (NaN) {
+    console.log("Truthy");
+
+} else {
+    console.log("Falsy");
+}
+
+
+if (1234) {
+    console.log("Truthy");
+
+} else {
+    console.log("Falsy");
+}
+
+if (null) {
+    console.log("Truthy");
+
+} else {
+    console.log("Falsy");
+}
+
+if (undefined) {
+    console.log("Truthy");
+
+} else {
+    console.log("Falsy");
+}
