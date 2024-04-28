3ee6a639ace95efee0ddc2ad612c652c23cbc52c
Chrystine Martins <chrystine.martins@outlook.com>
2024-04-28 13:24:57 -0300

    conditionals

---
+++ javascript/conditionals/app.js
@@ -0,0 +1,47 @@
+// let random = Math.random();
+
+// if (random < 0.5) {
+//     console.log('Your number is less than 0.5');
+
+// } else {
+//     console.log('Your number is greater than 0.5');    
+// }
+
+// console.log(random);
+
+
+// const dayOfWeek = prompt('Enter a week day').toLowerCase();
+
+// if (dayOfWeek === 'monday') {
+//     console.log('UGHH I HATE MONDAYS!');
+
+// } else if (dayOfWeek === 'saturday') {
+//     console.log('YAY I LOVE SATURDAYS!');
+
+// } else if (dayOfWeek === 'friday') {
+//     console.log('FRIDAYS ARE DECENT, ESPECIALLY AFTER WORK!');
+
+// } else {
+//     console.log('MEH');
+// }
+
+
+//0-5 FREE
+//5-10 CHILD $10
+//10-65 ADULT $20
+//65+ SENIOR $10
+
+const age = 34;
+
+if (age < 5) {
+    console.log('You are a baby. You get in for free!');
+
+} else if (age < 10) {
+    console.log('You are a child. You pay $10');
+
+} else if (age < 65) {
+    console.log('You are an adult. You pay $20');
+
+} else {
+    console.log('You are a senior. You pay $10');
+}
