dbe2979ba9ec755ea18aefecd7ea262776af7d1d
Chrystine Martins <chrystine.martins@outlook.com>
2024-04-29 20:04:44 -0300

    logical operators

--- javascript/logical-operators/index.js
+++ javascript/logical-operators/index.js
@@ -1,10 +1,51 @@
-//&&- and
+//&& - and
 
-let password = prompt("Enter your password");
+// let password = prompt("Enter your password");
 
-if (password.length > 6 && password.indexOf(' ') === -1) {
-    console.log("Valid password");
+// if (password.length > 6 && password.indexOf(' ') === -1) {
+//     console.log("Valid password");
 
-} else {
-    console.log("Invalid password");
+// } else {
+//     console.log("Invalid password");
+// }
+
+
+//|| - Or
+
+//0-5 free
+//5-10 $10
+//10-65 $20
+//65+ free
+
+// const age = -1;
+
+// if (age >= 0 && age < 5 || age >= 65) {
+//     console.log("FREE");
+
+// } else if (age >= 5 && age < 10) {
+//     console.log("$10");
+
+// } else if (age >= 10 && age < 65) {
+//     console.log("$20");
+
+// } else {
+//     console.log("Invalid Age");
+// }
+
+
+//! - Not
+
+// let firstName = prompt("Enter your first name");
+
+// if (!firstName) {
+//     firstName = prompt("Enter your first name");
+// }
+
+
+const age = 1;
+
+if (!(age >= 0 && age < 5 || age >= 65)) {
+    console.log("You are not a baby or a senior");
 }
+
+//se age não estiver dentro do intervalo de idade de 0 a 4 anos ou age não for maior ou igual a 65
