3a0cc320be69edc4bc81abec6e3828c6da62fb80
Chrystine Martins <chrystine.martins@outlook.com>
2024-05-01 10:28:09 -0300

    arrays

---
+++ javascript/modifying-arrays/index.js
@@ -0,0 +1,14 @@
+let colors = ['rad', 'orange', 'green', 'yelloow']
+
+console.log(colors.length)
+
+colors[0] = 'red'
+colors[3] = 'yellow'
+
+console.log(colors);
+
+console.log(colors[4])
+
+colors[4] = 'blue'
+
+console.log(colors);
