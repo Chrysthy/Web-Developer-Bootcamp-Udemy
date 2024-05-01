888ab6246184c061180ad0627db9ec6273740190
Chrystine Martins <chrystine.martins@outlook.com>
2024-04-30 21:10:44 -0300

    update arrays

--- javascript/arrays/index.js
+++ javascript/arrays/index.js
@@ -7,9 +7,3 @@ colors[0]; // "red"
 // They have a length:
 colors.length; //3
 
-// Important array methods:
-//push(value) - adds value to the END of an array
-//pop() - removes and returns last value in array
-
-//unshift(val) - adds value to START of an array
-//shift() - removes and returns first element in an array
