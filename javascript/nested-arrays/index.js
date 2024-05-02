3249f7b4c29a9147251f74e697cc496e6f8274b2
Chrystine Martins <chrystine.martins@outlook.com>
2024-05-02 20:52:58 -0300

    nested arrays

---
+++ javascript/nested-arrays/index.js
@@ -0,0 +1,10 @@
+const gameBoard = [
+    ['X', '0', 'X'],
+    ['O', null, 'X'],
+    ['O', 'O', 'X']
+]
+
+console.log(gameBoard);
+
+console.log(gameBoard[1][1]); //null
+//para acessar os dados de uma nested array, é só colocar os colchetes. 
