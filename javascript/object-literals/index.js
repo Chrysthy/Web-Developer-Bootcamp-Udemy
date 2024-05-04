2f2ec50a0c6700c576ecf6237bc0da7d923d823a
Chrystine Martins <chrystine.martins@outlook.com>
2024-05-04 20:13:00 -0300

    object literals

--- javascript/object-literals/index.js
+++ javascript/object-literals/index.js
@@ -16,3 +16,14 @@ dog["age"]; //7
 //updating values
 dog.breed = "mutt";
 dog["age"] = 8;
+
+
+//First Object Exercise
+
+let product = {
+    name: "Gummy Bears",
+    inStock: true,
+    price: 1.99,
+    flavors: ["grape", "apple", "cherry"]
+};
+
