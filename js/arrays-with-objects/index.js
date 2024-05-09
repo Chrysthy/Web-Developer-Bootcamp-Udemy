123866ad8beaae414f549bf9cc69722f555a733e
Chrystine Martins <chrystine.martins@outlook.com>
2024-05-09 20:27:46 -0300

    arrays with objects

---
+++ js/arrays-with-objects/index.js
@@ -0,0 +1,40 @@
+//array + objects
+
+const shoppingCard = [
+
+    {
+        product: 'Jenga Classic',
+        price: 6.88,
+        quantity: 1
+
+    },
+
+    {
+        product: 'Echo Dot',
+        price: 29.99,
+        quantity: 3
+    },
+
+    {
+        product: 'Fire Stick',
+        price: 39.99,
+        quantity: 2
+    }
+
+
+]
+
+console.log(shoppingCard[1]);
+console.log(shoppingCard[1].price);
+
+
+//objects + arrays
+
+const students = {
+
+    firstName: 'David',
+    lastName: 'Jones',
+    strengths:
+
+        ['Music', 'Art']
+}
