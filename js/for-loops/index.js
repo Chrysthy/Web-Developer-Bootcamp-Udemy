2e4b8179c7ad12a3910ecb6e59e8f120d7aeef89
Chrystine Martins <chrystine.martins@outlook.com>
2024-05-12 11:32:09 -0300

    loops

--- js/for-loops/index.js
+++ js/for-loops/index.js
@@ -7,6 +7,20 @@ for (let i = 1; i <= 10; i++) {
 
 // Print out "Da ba dee da ba daa" 6 times, using a for loop
 
-for (let i = 0; i < 6; i++){
+for (let i = 0; i < 6; i++) {
     console.log("Da ba dee da ba da")
+}
+
+
+//print even numbers
+//o 2 significa que são dois a mais do que tinha antes
+
+for (let i = 0; i <= 20; i += 2) {
+    console.log(i);
+}
+
+//print odd number
+
+for (i = 1; i <= 20; i += 2) {
+    console.log(i)
 }
