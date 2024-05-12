dd7f2194cbd4b4b6a155df8bd4c7b416dc13974d
Chrystine Martins <chrystine.martins@outlook.com>
2024-05-12 13:02:06 -0300

    loops

--- js/for-loops/index.js
+++ js/for-loops/index.js
@@ -24,3 +24,34 @@ for (let i = 0; i <= 20; i += 2) {
 for (i = 1; i <= 20; i += 2) {
     console.log(i)
 }
+
+//count down 100 to 0
+
+for (let i = 100; i >= 0; i -= 10) {
+    console.log(i)
+}
+
+
+//From 1 to 100
+for (let i = 0; i <= 100; i++) {
+    console.log(i)
+}
+
+
+for (let i = 10; i <= 100; i *= 10) {
+    console.log(i)
+}
+
+
+//count down 25 to 0
+for (let i = 25; i >= 0; i -= 5) {
+    console.log(i)
+}
+
+
+//Do not run this loop
+//infinite loop!! Bad!!
+
+// for (let i = 20; i >= 0; i++){
+//    console.log(i)
+//}
