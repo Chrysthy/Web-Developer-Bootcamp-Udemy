c370cd25a9127a9b8083b373d916772a7487cd80
Chrystine Martins <chrystine.martins@outlook.com>
2024-05-11 21:13:58 -0300

    loops

--- js/for-loops/index.js
+++ js/for-loops/index.js
@@ -1,3 +1,12 @@
+//i += 1 pode usar assim também
+
 for (let i = 1; i <= 10; i++) {
     console.log(i)
+}
+
+
+// Print out "Da ba dee da ba daa" 6 times, using a for loop
+
+for (let i = 0; i < 6; i++){
+    console.log("Da ba dee da ba da")
 }
