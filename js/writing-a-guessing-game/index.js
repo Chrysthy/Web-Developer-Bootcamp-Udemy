f629a41422106e325dc048b3eec3fdd8604ea0c3
Chrystine Martins <chrystine.martins@outlook.com>
2024-05-15 20:14:21 -0300

    guessing game

--- js/writing-a-guessing-game/index.js
+++ js/writing-a-guessing-game/index.js
@@ -1,7 +1,24 @@
 let maximum = parseInt(prompt("Enter the maximum number!"))
 
-if (!maximum) {
+while (!maximum) {
     maximum = parseInt(prompt("Enter a valid number!"))
 }
 
+
 const targetNum = Math.floor(Math.random() * maximum) + 1
+console.log(targetNum);
+
+
+let guess = parseInt(prompt("Enter your first guess!"))
+
+while (guess !== targetNum) {
+
+    if (guess > targetNum) {
+        guess = parseInt(prompt("Too hight! Enter a new guess:"))
+
+    } else {
+        guess = parseInt(prompt("Too low! Enter a new guess:"))
+    }
+}
+
+alert("You got it!");
