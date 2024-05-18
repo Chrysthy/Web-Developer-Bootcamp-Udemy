1100e2f1addfbd118389d6f1836c36bd2830c7fc
Chrystine Martins <chrystine.martins@outlook.com>
2024-05-18 15:30:44 -0300

    guessing game

--- js/writing-a-guessing-game/index.js
+++ js/writing-a-guessing-game/index.js
@@ -9,7 +9,7 @@ const targetNum = Math.floor(Math.random() * maximum) + 1
 console.log(targetNum);
 
 
-let guess = parseInt(prompt("Enter your first guess!"))
+let guess = prompt("Enter your first guess! (Type 'q' to quit the game")
 
 let attempts = 1;
 
@@ -17,14 +17,18 @@ while (parseInt(guess) !== targetNum) {
 
     if (guess === 'q') break;
 
+    guess = parseInt(guess)
 
     attempts++
 
     if (guess > targetNum) {
         guess = prompt("Too hight! Enter a new guess:")
 
-    } else {
+    } else if (guess < targetNum) {
         guess = prompt("Too low! Enter a new guess:")
+
+    } else {
+        guess = prompt("Invalid guess. Please enter a number or 'q' to quit.")
     }
 }
 
