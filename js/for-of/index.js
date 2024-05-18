31057b36cef9cb56d29e204a233285f8ef7853dd
Chrystine Martins <chrystine.martins@outlook.com>
2024-05-18 16:32:13 -0300

    for of

---
+++ js/for-of/index.js
@@ -0,0 +1,42 @@
+const subreddits = ['cringe', 'books', 'chickens', 'funny', 'pics', 'soccer', 'gunners']
+
+for (let i = 0; i < subreddits.length; i++) {
+
+    console.log(`Visit reddit.com/r${subreddits[i]}`);
+}
+
+for (let sub of subreddits) {
+    console.log(sub);
+}
+
+
+//o exemplo anterior de nested loops
+const seatingChart = [
+    ['Noob', 'Erik', 'Namita'],
+    ['Geoffrey', 'Juanita', 'Antonio', 'Kevin'],
+    ['Yuma', 'Sakura', 'Jack', 'Erika']
+]
+
+// for (let i = 0; i < seatingChart.length; i++) {
+//     const row = seatingChart[i];
+
+//     console.log(`Row #${i + 1}`);
+
+//     for (let j = 0; j < row.length; j++) {
+//         console.log(row[j]);
+//     }
+// }
+
+//usando for of
+
+for (let row of seatingChart) {
+
+    for (let student of row) {
+
+        console.log(student);
+    }
+}
+
+
+
+
