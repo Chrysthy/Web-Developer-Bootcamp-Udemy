13c40245846d87d6021c592711cc34f1dbf84378
Chrystine Martins <chrystine.martins@outlook.com>
2024-05-20 20:20:06 -0300

    exercises

---
+++ js/exercises/return-value-practice.js
@@ -0,0 +1,16 @@
+// It's time to practice returning values from a function! Write a simple function multiply which accepts two numerical arguments and returns their product (multiply them together).  Make sure to return the value instead of printing it!
+
+// multiply(2,3) // 6
+// multiply(9,9) // 81
+// multiply(5,4) // 20
+
+
+
+function multiply(x, y){
+    return x * y;
+    
+}
+
+multiply(2, 3);
+multiply(9, 9);
+multiply(5, 4);
