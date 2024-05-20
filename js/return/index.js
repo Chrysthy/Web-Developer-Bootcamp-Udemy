7e0684f6b2571409725d23a15816dcb0838de9ec
Chrystine Martins <chrystine.martins@outlook.com>
2024-05-20 20:01:03 -0300

    return keyword

--- js/return/index.js
+++ js/return/index.js
@@ -6,13 +6,39 @@
 // add(3, 3)
 
 //return
+// function add(x, y) {
+
+//     let sum = x + y;
+
+//     return sum;
+
+// }
+
+// add(5, 5)
+
+
+
+// function add(x, y) {
+
+//     let sum = x + y;
+
+//     return sum;
+
+//     //não irá funcionar depois do return
+//     console.log("end of dunction")
+// }
+
+// add(5, 5)
+
+
 function add(x, y) {
 
-    let sum = x + y;
+    if (typeof x !== 'number' || typeof y !== 'number') {
+        return false;
+    }
 
-    return sum;
+    return x + y
 
-    console.log("end of dunction")
 }
 
-add(5, 5)
+add('a', 5)
