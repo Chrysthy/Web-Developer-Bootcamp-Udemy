8e9abf3d1ad00d368634c3ae0094a01927c4de46
Chrystine Martins <chrystine.martins@outlook.com>
2024-05-25 15:46:37 -0300

    scopes

---
+++ js/scopes/lexical-scope.js
@@ -0,0 +1,16 @@
+function bankRobbery() {
+
+    const heros = ['Spiderman', 'Wolverine', 'Black Panther', 'Batwoman']
+
+    function cryForHelp() {
+
+        for (let hero of heros) {
+
+            console.log(`Please, help us, ${hero.toUpperCase()}`);
+
+        }
+
+    }
+
+    cryForHelp()
+}
