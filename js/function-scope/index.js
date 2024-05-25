641e5575fbf87f01cd8e3d12b7de927780174e1c
Chrystine Martins <chrystine.martins@outlook.com>
2024-05-25 15:12:14 -0300

    scopes

--- js/function-scope/index.js
+++ js/function-scope/index.js
@@ -6,12 +6,24 @@
 
 // console.log(totalEggs) //error - it is noe defined
 
-let totalEggs = 0
+// let totalEggs = 0
 
-function collectEggs() {
-    totalEggs = 6
+// function collectEggs() {
+//     totalEggs = 6
+// }
+
+// console.log(totalEggs);
+// collectEggs()
+// console.log(totalEggs);
+
+
+let bird = "Scarlet Macaw";
+
+function birdWatch() {
+    let bird = "Great Blue Heron"
+
+    console.log(bird);
 }
 
-console.log(totalEggs);
-collectEggs()
-console.log(totalEggs);
+birdWatch()
+console.log(bird);
