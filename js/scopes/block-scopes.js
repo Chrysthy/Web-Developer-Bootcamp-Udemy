d0a6834d2fe5d12d23edb3d6cf770d3dad7c0d95
Chrystine Martins <chrystine.martins@outlook.com>
2024-05-25 15:27:42 -0300

    scopes

---
+++ js/scopes/block-scopes.js
@@ -0,0 +1,19 @@
+// let radius = 8;
+
+// if (radius > 0) {
+//     const PI = 3.14159;
+//     let msg = 'Hi!'
+// }
+
+// console.log(radius);
+// console.log(msg);
+
+for (let i = 0; i < 5; i++) {
+
+    // let msg = "Hey!"
+    var msg = "Hey!"
+    console.log(msg);
+
+}
+
+console.log(msg);
