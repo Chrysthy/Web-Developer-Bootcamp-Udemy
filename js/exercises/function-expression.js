77ebe9af35eb209bb6f2a01d2c7acf569bc0cf3c
Chrystine Martins <chrystine.martins@outlook.com>
2024-05-26 15:47:33 -0300

    exercises

---
+++ js/exercises/function-expression.js
@@ -0,0 +1,15 @@
+// Function Expression Exercise
+// Define a function that returns the square of a number. Define the function as a function expression, stored in a variable called square.  
+
+// square(4) //16
+// square(3) //9
+
+
+let square = function (num) {
+    
+    return num * num
+    
+}
+
+square(4)
+square(3)
