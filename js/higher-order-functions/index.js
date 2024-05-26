e30b68de6ba2d356a3c4e4afa739933b7624b2f3
Chrystine Martins <chrystine.martins@outlook.com>
2024-05-26 16:16:17 -0300

    higher order functions

--- js/higher-order-functions/index.js
+++ js/higher-order-functions/index.js
@@ -5,8 +5,57 @@
 // greet()
 
 
-function callTwice(func) {
+// function callTwice(func) {
 
-    func()
-    func()
+//     func()
+//     func()
+// }
+
+
+// function callTenTimes(f) {
+
+//     for (let i = 0; i < 10; i++) {
+//         f()
+
+//     }
+// }
+
+// callTenTimes(rollDice)
+
+
+// function rollDice() {
+
+//     const roll = Math.floor(Math.random() * 6) + 1
+
+//     console.log(roll);
+
+// }
+
+// rollDice()
+
+
+// callTwice(rollDice)
+
+
+//******Return a Function*******
+function makeMysteryFunc() {
+
+    //numero aleatório
+    const randomNum = Math.random()
+
+    if (randomNum > 0.5) {
+        return function () {
+
+            console.log("Congrats, I'm a good function");
+            console.log("You win a Million Dollars!");
+        }
+
+
+    } else {
+        alert("You have been infected by a computer virus!");
+        alert("Stop trying to close this window!");
+
+    }
 }
+
+makeMysteryFunc()
