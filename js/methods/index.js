06756420e0fdfc0c541ecfb86f163982d3ed8c41
Chrystine Martins <chrystine.martins@outlook.com>
2024-05-30 14:33:48 -0300

    methods

--- js/methods/index.js
+++ js/methods/index.js
@@ -1,13 +1,28 @@
 //Defining methods
 
 //string method
-"hello".toUpperCase()
+// "hello".toUpperCase()
 
 //array method
-[1, 2, 3].indexOf(2)
+// [1, 2, 3].indexOf(2)
 
 //defining a function
-function add(x, y) {
-	return x + y
+// function add(x, y) {
+// 	return x + y
+// }
+
+const myMath = {
+
+	PI: 3.14159,
+	square: function (num) {
+		return num * num
+
+	},
+
+	cube: function (num) {
+		return num ** 3  //significa elevado a potência
+
+	}
 }
 
+console.log(myMath.PI)
