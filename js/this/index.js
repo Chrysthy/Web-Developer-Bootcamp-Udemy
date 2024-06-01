02fc07b533c0bdf59486be96cddd32d1ac2f7c5d
Chrystine Martins <chrystine.martins@outlook.com>
2024-06-01 16:08:35 -0300

    this

--- js/this/index.js
+++ js/this/index.js
@@ -5,12 +5,17 @@ const cat = {
 
     meow() {
 
-        console.log(this.color);
-        console.log(`${this.name} says MEOW`);
-        console.log(this.name);
+        // console.log(this.color);
+        console.log("This is: ", this);
+        // console.log(`${this.name} says MEOW`);
+        // console.log(this.name);
     }
 }
 
 //A palavra this irá se referir em vez do método, ao próprio objeto: cat
 
 cat.meow()
+
+let meow2 = cat.meow
+
+meow2()
