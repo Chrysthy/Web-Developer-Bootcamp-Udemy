f4f811299dd7c3058000b942b0577bee02af804d
Chrystine Martins <chrystine.martins@outlook.com>
2024-06-18 15:12:08 -0300

    update content

--- js/map/index.js
+++ js/map/index.js
@@ -1,23 +1,10 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 
-//não comum
-// function print(element) {
-//     console.log(element);
-// }
-
-//muito comum
-numbers.forEach(function (el) {
-
-    if (el % 2 === 0) {
-
-        console.log(el);
-    }
-
+const doubles = numbers.map(function (num) {
+    return num * 2
 })
 
-// for (let el of numbers) {
-//     console.log(el);
-// }
+console.log(doubles);
 
 
 const movies = [
@@ -45,7 +32,8 @@ const movies = [
 
 ]
 
-movies.forEach(function (movie) {
-    console.log(`${movie.title} - ${movie.score} /100)`);
+const titles = movies.map(function (movie) {
+    return movie.title.toUpperCase()
 })
 
+console.log(titles);
