bc707b6fbcb097261eb2e080ca99f0b329af5479
Chrystine Martins <chrystine.martins@outlook.com>
2024-06-18 16:03:21 -0300

    update content

---
+++ js/arrow-functions/index.js
@@ -0,0 +1,26 @@
+//normal function
+// const add = function (x, y) {
+//     return x + y
+// }
+
+
+//arrow function
+const add = (x, y) => {
+    return x + y
+}
+
+console.log(add(9, 4));
+
+
+const square = (x) => {
+    return x * x
+}
+
+console.log(square(9));
+
+
+const rollDie = () => {
+    return Math.floor(Math.random() * 6) + 1
+}
+
+console.log(rollDie());
