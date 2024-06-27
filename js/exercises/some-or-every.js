1ba80d5f445197ea52d49ff52b139da3ab4f0b73
Chrystine Martins <chrystine.martins@outlook.com>
2024-06-27 20:32:09 -0300

    some and every exercise

--- js/exercises/some-or-every.js
+++ js/exercises/some-or-every.js
@@ -1,7 +1,13 @@
 // Some/Every Exercise
 // Define a function called allEvens that accepts a single array of numbers.  If the array contains all even numbers, return true.  Otherwise, return false.  Use some or every to help you do this!  (only one of them is actually useful here)
 
+
+function allEvens (numbers) {
+    return numbers.every(function (num) {
+        return num % 2 === 0
+    }); 
+}
+
 allEvens([2,4,6,8]) //true
 allEvens([1,4,6,8]) //false
 allEvens([1, 2, 3]) //false
-
