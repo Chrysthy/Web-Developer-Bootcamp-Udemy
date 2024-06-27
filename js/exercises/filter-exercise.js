8761bf146c72202d75da3285bbe778678ee2602e
Chrystine Martins <chrystine.martins@outlook.com>
2024-06-27 19:34:35 -0300

    filter exercise

--- js/exercises/filter-exercise.js
+++ js/exercises/filter-exercise.js
@@ -1,9 +1,9 @@
 // Let's get some practice using the filter method. Write a function called validUserNames that accepts an array of usernames (strings).  It should return a new array, containing only the usernames that are less than 10 characters.
 
-let validUserNames = ['mark', 'staceysmom1978', 'q29832128238983', 'carrie98', 'MoanaFan']
+let usernames = ['mark', 'staceysmom1978', 'q29832128238983', 'carrie98', 'MoanaFan']
 
 function validUserNames(usernames) {
-    
+
     return usernames.filter(name => name.length < 10)
 
 }
