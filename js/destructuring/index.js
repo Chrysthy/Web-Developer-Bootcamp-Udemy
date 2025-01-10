730bca8d72e859a8792b7077e3a44a614f04da32
Chrystine Martins <chrystine.martins@outlook.com>
2025-01-10 11:30:41 -0300

    add example

--- js/destructuring/index.js
+++ js/destructuring/index.js
@@ -1,2 +1,13 @@
 //destructuring Arrays
 //descompactar ou extrair valores de um objeto ou array e podemos descompactá-los e colocá-los em variáveis separadas
+
+//Não estamos removando e sim copiamos o valor para uma nova variável
+
+const scores = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+
+const [gold, silver, bronze, ...everyoneElse] = scores;
+
+console.log(`Gold medal goes to: ${gold}`);
+console.log(`Silver medal goes to: ${silver}`);
+console.log(`Bronze medal goes to: ${bronze}`);
+console.log(`Thanks to everyone else: ${everyoneElse}`);
