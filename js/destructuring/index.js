e8fa6168d558075d4b22424a7575ef877c7a0420
Chrystine Martins <chrystine.martins@outlook.com>
2025-01-10 11:31:53 -0300

    update code

--- js/destructuring/index.js
+++ js/destructuring/index.js
@@ -7,7 +7,7 @@ const scores = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
 
 const [gold, silver, bronze, ...everyoneElse] = scores;
 
-console.log(`Gold medal goes to: ${gold}`);
-console.log(`Silver medal goes to: ${silver}`);
-console.log(`Bronze medal goes to: ${bronze}`);
-console.log(`Thanks to everyone else: ${everyoneElse}`);
+console.log(gold);
+console.log(silver);
+console.log(bronze);
+console.log(everyoneElse);
